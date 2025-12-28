b4675f5215fb9fdd78905a68981d9bf9baed1150
Ram Madhav Bommaraboyina <b.rammadhav@gmail.com>
2025-12-28 21:27:43 +0530

    done

--- k.py
+++ k.py
@@ -6,7 +6,7 @@ import csv  # New library for handling CSV files
 url = "https://www.commoditymarketlive.com/"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
+}d
 
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
